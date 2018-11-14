595c5a2411277dff1412c67a5b2bddaef00de00c
myidispg <myidispg@gmail.com>
2018-11-14 14:06:54 +0530

    Working on CNN for Cifar-10

--- Part 2 - CNN/cifar-10-cnn.py
+++ Part 2 - CNN/cifar-10-cnn.py
@@ -35,4 +35,92 @@ test_data = datasets.CIFAR10('data', train=False, download=True, transform=trans
 
 # obtain indices used for validation
 num_train = len(train_data)
-indices = list()
+indices = list(range(num_train))
+np.random.shuffle(indices)
+split = int(np.floor(valid_size * num_train))
+train_idx, valid_idx = indices[split:], indices[:split]
+
+# Define sampler for obtaining training and validation batches
+train_sampler = SubsetRandomSampler(train_idx)
+valid_sampler = SubsetRandomSampler(valid_idx)
+
+# prepare data loaders(combine dataset and sampler)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size = batch_size,
+                                           sampler = train_sampler, num_workers=num_workers)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size,
+                                        sampler=test_sampler, num_workers=num_workers)
+
+# Specify the image classes
+classes=['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog',
+         'horse', 'ship', 'truck']
+# -------Visualize the data---------------
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+# helper function to un-normalize and display the image
+def imshow(img):
+    img = img/2 + 0.5
+    plt.imshow(np.transpose())
+
+# obtain one batch of training images
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+images = images.numpy() # convert images to numpy for display
+
+# plot the images in the batch, along with the corresponding labels
+fig = plt.figure(figsize=(25, 4))
+# display 20 images
+for idx in np.arange(20):
+    ax = fig.add_subplot(2, 20/2, idx+1, xticks=[], yticks=[])
+    imshow(images[idx])
+    ax.set_title(classes[labels[idx]])
+    
+rgb_img = np.squeeze(images[3])
+channels = ['red channel', 'green channel', 'blue channel']
+
+fig = plt.figure(figsize = (36, 36)) 
+for idx in np.arange(rgb_img.shape[0]):
+    ax = fig.add_subplot(1, 3, idx + 1)
+    img = rgb_img[idx]
+    ax.imshow(img, cmap='gray')
+    ax.set_title(channels[idx])
+    width, height = img.shape
+    thresh = img.max()/2.5
+    for x in range(width):
+        for y in range(height):
+            val = round(img[x][y],2) if img[x][y] !=0 else 0
+            ax.annotate(str(val), xy=(y,x),
+                    horizontalalignment='center',
+                    verticalalignment='center', size=8,
+                    color='white' if img[x][y]<thresh else 'black')
+            
+# -------Define the neural network
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Network(nn.module):
+    
+    def __init__(self):
+        super(Network, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, 3)
+        self.conv2 = nn.Conv2d(16, 32, 3)
+        self.conv3 = nn.Conv2d(32, 64, 3)
+        self.pool = nn.MaxPool2d(2,2)
+        
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1))
+        
+        return x.size
+    
+model = Network()
+
+if train_on_gpu:
+    model.cuda()
+    
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.03)
+
+        
+        
