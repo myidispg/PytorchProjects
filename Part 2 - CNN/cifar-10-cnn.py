2b8ae00f8ee92a232b2030e25beae2ffbe5b9edd
myidispg <myidispg@gmail.com>
2018-11-14 18:10:53 +0530

    CNN for Cifar-10

--- Part 2 - CNN/cifar-10-cnn.py
+++ Part 2 - CNN/cifar-10-cnn.py
@@ -102,7 +102,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 # Input image is 32x32x3
-class Network(nn.module):
+class Network(nn.Module):
     
     def __init__(self):
         super(Network, self).__init__()
@@ -119,22 +119,22 @@ class Network(nn.module):
         # 500 -> 10
         self.fc2 = nn.Linear(500, 10)
         
-        self.dropout(0.25)
+        self.dropout = nn.Dropout(0.25)
         
     def forward(self, x):
-        x = self.pool(self.relu(self.conv1)) # output = 31x31x16 -> 16x16x16
-        x = self.pool(self.relu(self.conv2)) # output = 15x15x32 -> 8x8x32
-        x = self.pool(self.relu(self.conv3)) # output = 7x7x64 -> 4x4x64
+        x = self.pool(F.relu(self.conv1(x))) # output = 31x31x16 -> 16x16x16
+        x = self.pool(F.relu(self.conv2(x))) # output = 15x15x32 -> 8x8x32
+        x = self.pool(F.relu(self.conv3(x))) # output = 7x7x64 -> 4x4x64
         # Flattern the image output
         x = x.view(-1, 64*4*4)
-        x = self.dropout(self.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc1(x)))
         x = self.fc2(x)
-        return x.size
+        return x
     
 model = Network()
 
-if train_on_gpu:
-    model.cuda()
+#if train_on_gpu:
+#    model.cuda()
     
 import torch.optim as optim
 
@@ -146,30 +146,30 @@ n_epochs = 30
 
 valid_loss_min = np.Inf # Give infinite value to valid_loss_min. Used later.
 
- for epoch in range(1, n_epochs+1):
+for epoch in range(1, n_epochs+1):
+        
+    train_loss = 0
+    valid_loss = 0
      
-     train_loss = 0
-     valid_loss = 0
-     
-     # set the model to train mode
-     model.train()
-     for data, target in train_loader:
-         if train_on_gpu:
-             data, target = data.cuda(), target.cuda()
-         optimizer.zero_grad()
-         output = model(data)
-         loss = criterion(output, target)
-         loss.backward()
-         optimizer.step()
-         train_loss += loss.item()*data.size[0]
-    
-     model.eval()
-     for data, target in valid_loader:
-         if train_on_gpu:
-             data, target = data.cuda(), target.cuda()
+    # set the model to train mode
+    model.train()
+    for data, target in train_loader:
+#        if train_on_gpu:
+#            data, target = data.cuda(), target.cuda()
+        optimizer.zero_grad()
         output = model(data)
         loss = criterion(output, target)
-        valid_loss += loss.item()*data.size[0]
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()*data.size(0)
+    
+    model.eval()
+    for data, target in valid_loader:
+#        if train_on_gpu:
+#            data, target = data.cuda(), target.cuda()
+        output = model(data)
+        loss = criterion(output, target)
+        valid_loss += loss.item()*data.size(0)
         
     # Calculate average loss
     train_loss /= len(train_loader.dataset)
@@ -189,4 +189,66 @@ valid_loss_min = np.Inf # Give infinite value to valid_loss_min. Used later.
 
 # Load the model with lowest validation loss
 model.load_state_dict(torch.load('model_cifar.pt'))
+
+# Test the trained network
+test_loss = 0.0
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+
+model.eval()
+
+for data, target in test_loader:
+    if train_on_gpu:
+        data, target = data.cuda(), target.cuda()
         
+    output = model(data)
+    loss = criterion(output, target)
+    test_loss += loss.item()*data.size(0)
+    _, pred = torch.max(output, 1)
+    
+    correct_tensor = pred.eq(target.data.view_as(pred))
+    correct = np.squeeze(correct_tensor.numpy()) if not train_on_gpu else np.squeeze(correct_tensor.cpu().numpy())
+    # calculate test accuracy for each object class
+    for i in range(batch_size):
+        label = target.data[i]
+        class_correct[label] += correct[i].item()
+        class_total[label] += 1
+
+# average test loss
+test_loss = test_loss/len(test_loader.dataset)
+print('Test Loss: {:.6f}\n'.format(test_loss))
+for i in range(10):
+    if class_total[i] > 0:
+        print('Test Accuracy of %5s: %2d%% (%2d/%2d)' % (
+            classes[i], 100 * class_correct[i] / class_total[i],
+            np.sum(class_correct[i]), np.sum(class_total[i])))
+    else:
+        print('Test Accuracy of %5s: N/A (no training examples)' % (classes[i]))
+
+print('\nTest Accuracy (Overall): %2d%% (%2d/%2d)' % (
+    100. * np.sum(class_correct) / np.sum(class_total),
+    np.sum(class_correct), np.sum(class_total)))
+
+# ------Visualize the sample test results-------------------------------
+# obtain one batch of test images
+dataiter = iter(test_loader)
+images, labels = dataiter.next()
+images.numpy()
+
+# move model inputs to cuda, if GPU available
+if train_on_gpu:
+    images = images.cuda()
+
+# get sample outputs
+output = model(images)
+# convert output probabilities to predicted class
+_, preds_tensor = torch.max(output, 1)
+preds = np.squeeze(preds_tensor.numpy()) if not train_on_gpu else np.squeeze(preds_tensor.cpu().numpy())
+
+# plot the images in the batch, along with predicted and true labels
+fig = plt.figure(figsize=(25, 4))
+for idx in np.arange(20):
+    ax = fig.add_subplot(2, 20/2, idx+1, xticks=[], yticks=[])
+    imshow(images[idx])
+    ax.set_title("{} ({})".format(classes[preds[idx]], classes[labels[idx]]),
+                 color=("green" if preds[idx]==labels[idx].item() else "red"))
